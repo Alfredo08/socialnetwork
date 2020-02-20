296df3a6d780d3daa907faec45990634249a0e8a
Alfredo Salazar <>
2020-02-20 11:46:56 -0600

    Added encryption

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require( 'body-parser' );
 const knex = require( 'knex' );
 const xss = require( 'xss' );
 const sqlString = require( 'sqlstring' );
+const bcrypt = require( 'bcryptjs' );
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -17,31 +18,79 @@ app.post( '/api/login', jsonParser, ( req, res ) => {
     let { username, password } = req.body;
 
     console.log( username, password );
-    let t = database.raw(`SELECT * FROM users WHERE username=${sqlString.escape(username)} AND password = ${sqlString.escape(password)}`)
-    /*let t = database
+    //let t = database.raw(`SELECT * FROM users WHERE username=${sqlString.escape(username)} AND password = ${sqlString.escape(password)}`)
+    let t = database
         .select( '*' )
         .from( 'users' )
         .where({
-            username,
-            password
-        }) */
+            username
+        })
     //console.log(t.toQuery());
     //console.log( 'xss', xss( t.toQuery()) );
-    console.log( 'sqlstring', sqlString.escape( t.toQuery()) );
-    //console.log( t.toSQL().toNative());
-        t.then( result => {
+    //console.log( 'sqlstring', sqlString.escape( t.toQuery()) );
+    console.log( t.toSQL().toNative());
+    t.then( result => {
 
-            //if( result.length > 0 ){
-                return res.status(200).json({user: result})
-            //}
+        if( result.length > 0 ){
+            bcrypt.hash(password, 10)
+                .then( newpass => {
+                    console.log("DB", result[0].password);
+                    console.log("New", newpass);
+                })
+            bcrypt.compare(password, result[0].password)
+                .then( compareResult => {
+                    console.log( compareResult );
+                    if ( compareResult ){
+                        return res.status(200).json({message : `Welcome back ${username}` })
+                    }
+                    else{
+                        throw new Error( "Please verify your credentials");
+                    }
+                    
+                })
+                .catch( err => {
+                    res.statusMessage = err;
+                    return res.status(404).send();
+                });
+        }
+        else{
+            throw new Error( "That user doesn't exist! ");
+        }
 
-            //throw new Error( "That user doesn't exist! ");
-        })
-        .catch( err => {
-            res.statusMessage = "That user doesn't exist!";
-            return res.status(404).send();
+
+    })
+    .catch( err => {
+        res.statusMessage = err;
+        return res.status(404).send();
+    });
+
+});
+
+app.post( '/api/create-user', jsonParser, ( req, res ) => {
+
+    let { username, password } = req.body;
+
+    bcrypt.hash( password, 10 )
+        .then( hashPassword => {
+            console.log( hashPassword );
+            let newUser = {
+                username,
+                password : hashPassword
+            }
+
+            database
+                .insert( newUser )
+                .into( 'users' )
+                .returning('*')
+                .then( userCreated => {
+                    console.log( userCreated );
+                    return res.status(200).json(userCreated);
+                })
+                .catch( err => {
+                    res.statusMessage = "Something wen wrong. Try again later.";
+                    return res.status(404).send();
+                });
         });
-
 });
 
 app.listen( 8080, () =>{
